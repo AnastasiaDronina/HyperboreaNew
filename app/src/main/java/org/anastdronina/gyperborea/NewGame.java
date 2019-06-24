a3329ecdd527be2f55341d1879de9f12834c9ca7
Anastasia Dronina <43493089+lovely42hyena@users.noreply.github.com>
2019-06-24 15:20:12 +0300

    Добавила возможность скопировать email, написать письмо

--- app/src/main/java/org/anastdronina/gyperborea/NewGame.java
+++ app/src/main/java/org/anastdronina/gyperborea/NewGame.java
@@ -1,5 +1,6 @@
 package org.anastdronina.gyperborea;
 
+import android.content.ClipboardManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -91,6 +93,8 @@ public class NewGame extends AppCompatActivity implements View.OnClickListener {
                 }
             }
         });
+
+        TextView tvGameOver = findViewById(R.id.tvGameOver);
 
         dialogGameOver = new AlertDialog.Builder(this, R.style.MyDialogTheme).create();
         dialogGameOver.getWindow().getAttributes().windowAnimations = R.style.MyDialogTheme;
