2951b3465aeae9c4540a3b1fb615241d8f48bc78
Anastasia Dronina <43493089+lovely42hyena@users.noreply.github.com>
2019-06-24 15:05:52 +0300

    исправила background

--- app/src/main/java/org/anastdronina/gyperborea/Popup.java
+++ app/src/main/java/org/anastdronina/gyperborea/Popup.java
@@ -2,6 +2,7 @@ package org.anastdronina.gyperborea;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
